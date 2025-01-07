b05b49b5e9c08a77e1ba38db1f63ef955962cdd7
Gigiuz <sassaroluigi@gmail.com>
2025-01-07 14:29:56 +0100

    feat: test for dausyUI5 working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 export default function Home() {
   return (
-    <h1>Hello world!</h1>
+    <>
+      <h1>Hello world!</h1>
+      <button className="btn btn-primary">Button</button>
+    </>
   );
 }
